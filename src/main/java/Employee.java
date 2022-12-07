a10fed676c1563b7af78541e7976e8d7814fbf55
g00374265 <michael.e.agbo>
2022-12-07 11:24:48 +0000

    created employee constructor

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -6,5 +6,13 @@ public class Employee {
     private String gender;
     private int age;
 
+    public Employee(String name, long id, String employmentType, String gender, int age){
+    setName(name);
+    setId(id);
+    setEmployment(employmentType);
+    setGender(gender);
+    setAge(age);
+    }
+
 
 }
